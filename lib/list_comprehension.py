b7e35b7ea477026930c066691ffb804bea864712
zinakalu <zina.kalu@gmail.com>
2023-04-03 07:43:57 -0400

    lc

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = [num for num in num_list if num % 2 == 0]
+    return even
+# for num in num_list:
+    #     if num % 2 == 0:
+    #         num.append()
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [i+"!" for i in sentence_list]
+    return new_list
+
